2412c4fdd854843cdf895a87b025f3fea1cac067
Muhammad Nur Farizky <fariz@seru.app>
2025-11-04 11:47:19 +0700

    materi find + query dan penugasan fbs dan sql

--- p8/main.py
+++ p8/main.py
@@ -5,13 +5,24 @@ mydb = client['mydb'] #  Inisiasi database
 users = mydb['users']
 
 if __name__ == '__main__':
-    nama = input('masukkan nama: ')
-    umur = input('masukkan umur: ')
+    # nama = input('masukkan nama: ')
+    # umur = input('masukkan umur: ')
 
-    mydict = {
-        'nama': nama,
-        'umur': umur
-    }
+    # mydict = {
+    #     'nama': nama,
+    #     'umur': umur
+    # }
 
-    data = users.insert_one(mydict)
-    print(data.inserted_id)
+    # data = users.insert_one(mydict)  #  Memasukkan data/record ke collection
+
+    data = users.find_one() #  Mencari satu record ke collection, diambil nilai pertama
+
+    data = users.find() #  Mencari lebih dari  satu record, sehingga perlu diiterasi untuk mendapatkan datanya
+    # for d in data:
+    #     print(d)
+
+    query = {"umur": 20}  # case sensitive, "20" tidak sama dengan 20
+    data = users.find(query)  # query data
+
+    for d in data:
+        print(d)
