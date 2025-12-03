9b06e14fa258f7aa26b932b5a6799c1fec3c52fa
Muhammad Nur Farizky <fariz@seru.app>
2025-12-03 16:19:59 +0700

    tidying analyze.bat file

--- p10/analyze.bat
+++ p10/analyze.bat
@@ -1,4 +1,5 @@
-#!/bin/bash
+@REM #!/bin/
+@echo off
 
 @REM 1. Create HDFS folder for input/output
 docker-compose exec -it namenode hdfs dfs -mkdir -p /user/student
@@ -18,4 +19,4 @@ docker-compose exec -it namenode hadoop jar /data/tools/hadoop-streaming-2.7.3.j
 docker-compose exec -it namenode hdfs dfs -get -f /user/student/output/part-00000 /data/output.txt
 
 echo "=== OUTPUT ==="
-cat /data/output.txt
+docker-compose exec -it namenode cat /data/output.txt
