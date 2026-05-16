51744121983c2260ca83057c43930687d9f8e9e1
Muhammad Nur Farizky <fariz@seru.app>
2026-05-16 11:58:42 +0700

    while statement

--- 124/p7/main.py
+++ 124/p7/main.py
@@ -4,6 +4,8 @@
 # Memberikan notifikasi sebanyak 10x
 # Gunakan "for" statement untuk meringkas program yang bersifat repetitif
 
+import random
+
 # basic
 def for_loop1():
     for i in range(0, 10):
@@ -45,7 +47,65 @@ def for_loop3():
         print(f'{i + 1}. {list_kota[i]["kota"]}')
 
 # cara 2: iterasi variabel secara langsung
-n = 0
-for kota in list_kota:
-    n = n + 1 # Increment
-    print(f'{n}. {kota["kota"]} (jumlah kelurahan = {kota["jumlah_kelurahan"]})')
+def for_loop4():
+    n = 0
+    for kota in list_kota:
+        n = n + 1 # Increment
+        print(f'{n}. {kota["kota"]} (jumlah kelurahan = {kota["jumlah_kelurahan"]})')
+
+# fungsi ini menghasilkan list angka random
+# cara 1: sediakan list kosong, kemudian ditambah menggunakan append
+def generate_random1(count: int):
+    nums = []
+
+    for i in range(0, count):
+        nums.append(random.randint(0,100))
+
+    return nums
+
+# cara 2: langsung diiterasikan pada return
+def generate_random2(count: int):
+    return [
+        {
+            'index': i,
+            'value': random.randint(0, 100)
+        }
+        for i in range(0, count)
+    ]
+
+# while loop, lebih cocok untuk proses komputasi
+def while_loop1():
+    n = 0
+
+    # ubah nilai n menjadi 7, namun lakukan dengan cara incremental
+    while n < 7:
+        n = n + 1
+        print(n)
+
+# continue untuk skip iterasi
+def continue_func():
+    n = 0
+    for i in range(0,5):
+        n = n + 1
+        if i == 2:
+            continue
+        print(n)
+
+# Iterasi tanpa henti
+while True:
+    print('Selamat datang di aplikasi X')
+    print('1. Tampilkan data')
+    print('2. Masukkan data')
+    print('3. Analisis')
+    print('4. Selesai')
+
+    menu = input('Masukkan program yang akan dijalankan: ')
+    if menu == '1':
+        print('Data tampil')
+        # isi source code sesuai dengan tujuan program/fitur
+    elif menu == '2':
+        data = input('Masukkan data: ')
+        ###
+    elif menu == '4':
+        break # berfungsi untuk menghentikan iterasi
+
